7af9ba7c139f3abbcc26667e1798a2a3c8bd5ae3
Jean-Pierre Morfin <jpmorfin@befox.fr>
2017-05-02 22:35:56 +0200

    add jdenticon

--- sharewriting.js
+++ sharewriting.js
@@ -10,19 +10,32 @@
  * 
  ***************************************************/
 
-var express = require('express')
+/**
+ * To change port : npm config set sharewriting:port 3000
+ * default is 4984
+ */
+var port = process.env.npm_package_config_port || 4984;
+
+var express = require('express');
 
 var app = express();
 
 /* external libraries */
-app.use('/jquery', express.static('node_modules/jquery/dist'));
-app.use('/bootstrap', express.static('node_modules/bootstrap/dist'));
-app.use('/jdenticon', express.static('node_modules/jdenticon/dist'));
-app.use('/ckeditor', express.static('node_modules/ckeditor'));
-app.use('/tweetnacl', express.static('node_modules/tweetnacl'));
-app.use('/tweetnacl-util', express.static('node_modules/tweetnacl-util'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/jdenticon', express.static(__dirname + '/node_modules/jdenticon/dist'));
+app.use('/ckeditor', express.static(__dirname + '/node_modules/ckeditor'));
+app.use('/tweetnacl', express.static(__dirname + '/node_modules/tweetnacl'));
+app.use('/tweetnacl-util', express.static(__dirname + '/node_modules/tweetnacl-util'));
+app.use('/ckeditor-extra-plugins', express.static(__dirname + '/ckeditor-extra-plugins'));
 
 /* static files */
-app.use('/static', express.static('static'));
+app.use('/static', express.static(__dirname + '/static'));
 
 /* template engine */
+
+
+/* listening */
+app.listen(port, function () {
+  console.log('sharewriting app listening on port ' + port);
+});
---
+++ static/js/sharewriting-browser.js
@@ -0,0 +1,230 @@
+
+var ShareWriting = new ShareWritingClass();
+
+function ShareWritingClass() {
+	this.author_name = '';
+	this.keyPair ='';
+	
+	this.hexEncodeArray = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+	
+	/**
+	 * Initiate author key pairs
+	 */
+	this.setAuth = function(author_name, passphrase) {
+		this.author_name = author_name;
+		var hash512 = nacl.hash(nacl.util.decodeUTF8('[' + author_name + ':' + passphrase + ']'));
+		var seed = new Uint8Array(nacl.sign.seedLength);
+		for (var i = 0; i < 32; i++) seed[i] = hash512[i];
+		this.keyPair = nacl.sign.keyPair.fromSeed(seed);
+		return true;
+	};
+	
+	this.toHex = function(iArray) {
+		var s = '';
+		for (var i = 0; i < iArray.length; i++) {
+		var code = iArray[i];
+			s += this.hexEncodeArray[code >>> 4];
+			s += this.hexEncodeArray[code & 0x0F];
+		}
+		return s;
+	};
+	
+	/**
+	 * convert base64 encoded strinf to base36 encoded string
+	 */
+	this.b64toHex = function(s64) {
+		return this.toHex(nacl.util.decodeBase64(s64));
+	};
+	
+	/**
+	 * get base64 public key
+	 */
+	this.getPubKey = function() {
+		return nacl.util.encodeBase64(this.keyPair.publicKey);
+	};
+	
+	/**
+	 * get hex public key
+	 */
+	this.getPubKeyHex = function() {
+		return this.toHex(this.keyPair.publicKey);
+	};
+	
+	/**
+	 * get base64 message signature
+	 */
+	this.getSignature = function(jsonArticle) {
+		var signature = nacl.sign.detached(nacl.util.decodeUTF8(jsonArticle), this.keyPair.secretKey);
+		return nacl.util.encodeBase64(signature);
+	};
+	
+	/**
+	 * get html formatted writing
+	 */
+	this.getFormattedWriting = function(writing) {
+		var pubdate = new Date(writing.article.timestamp);
+		var rTags = writing.article.tags.split(',');
+		var html='<article>';
+		html += '<header>';
+		html += '<canvas class="jdenticon" style="float:right" width="100" height="100"';
+		html += ' data-jdenticon-hash="'+this.b64toHex(writing.article.pubkey)+'"';
+		html += ' alt="'+writing.article.author+'\'s pub key"';
+		html += ' title="'+writing.article.author+'\'s pub key"';
+		html += ' ></canvas>';
+		html += '<h1 class="headline">' + writing.article.title + '</h1>';
+		html += '<div class="byline">';
+		html += '<address class="author">By ' + writing.article.author + ' / ' + writing.article.pubkey + '</address>';
+		html += '<time pubdate datetime="' + pubdate.toISOString() + '" title="' + pubdate.toDateString()  + '">On ' + pubdate.toDateString() + '</time>';
+		html += '</div>';
+		if(rTags.length>0) {
+			html += '<ul class="tags">';
+			for (i = 0; i < rTags.length; i++) {
+				html += '<li class="tag"> #' + rTags[i] + '</li>';
+			}
+			html += '</ul>';
+		}
+		html += '</header>';
+		html += '<div class="article-content">' + writing.article.body + '</div>';
+		html += '<footer>';
+		html += '<div class="signature">' + writing.signature + '</div>';
+		html += '</footer>';
+		html += '</article>';
+		return html;
+	};
+	
+}
+
+$(function(){
+	/* init input fields */
+	$('input').val('');
+	
+	/* user lang */
+	var userLang = navigator.language || navigator.userLanguage;
+	$('#sw_lang').val(userLang);
+
+	// CKEDITOR.config.language = userLang;
+	CKEDITOR.plugins.addExternal( 'imageresize', '../ckeditor-extra-plugins/imageresize/' );
+	CKEDITOR.plugins.addExternal( 'base64image', '../ckeditor-extra-plugins/base64image/' );
+
+	var sw_editor = CKEDITOR.replace( 'sw_editor', {
+		toolbarGroups: [
+			{ name: 'styles', groups: [ 'styles' ] },
+			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+			{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+			{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+			{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+			{ name: 'links', groups: [ 'links' ] },
+			{ name: 'insert', groups: [ 'insert' ] },
+			{ name: 'forms', groups: [ 'forms' ] },
+			{ name: 'tools', groups: [ 'tools' ] },
+			{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+			{ name: 'others', groups: [ 'others' ] },
+			{ name: 'colors', groups: [ 'colors' ] },
+			{ name: 'about', groups: [ 'about' ] }
+		],
+		removeButtons: 'Cut,Copy,Paste,PasteText,PasteFromWord,Scayt,Maximize,Undo,Redo,Styles,About,Image,SpecialChar',
+		extraPlugins: 'dialogadvtab,tableresize,tabletools,imageresize,base64image',
+		removeDialogTabs : 'image:advanced;link:advanced;table:advanced',
+		format_tags: 'p;h2;h3;h4;h5;h6;pre',
+		forcePasteAsPlainText: true,
+		width: '100%',
+		height: '400',
+		contentsCss: [ '../bootstrap/css/bootstrap.min.css' ],
+		defaultLanguage: 'en',
+		language: userLang,
+		imageResize: []
+		//imageResize.maxWidth: 200,
+		//imageResize.maxHeight: 10000
+	} );
+		
+	$('#sw_lang').change(function(){
+		var userLang = $('#sw_lang').val();
+		var html = CKEDITOR.instances.sw_editor.getData();
+		CKEDITOR.instances.sw_editor.destroy();
+		CKEDITOR.replace( 'sw_editor' );
+		CKEDITOR.config.language = userLang;
+		CKEDITOR.instances.sw_editor.setData(html);
+	});
+
+	/* discard alert message */
+	$('button').click(function(){
+		$('#alert-error-message').hide();
+		$('#alert-error-content').html('');
+	});
+	
+	/* authentication */
+	$('#btn-authenticate').click(function(){
+		/* TODO: check author name and passphrase format */
+		if($('#sw_author').val().length<3) {
+			$('#alert-error-content').html('Author name must have at least 3 characters.');
+			$('#alert-error-message').show();
+			return;
+		}
+		if($('#sw_pass').val().length<10) {
+			$('#alert-error-content').html('Your pass phrase is too short to be secured : at least 10 characters.');
+			$('#alert-error-message').show();
+			return;
+		}
+		var result = ShareWriting.setAuth($('#sw_author').val(), $('#sw_pass').val());
+		if(result === true) {
+			$('#sw_pubkey').val(ShareWriting.getPubKey());
+			$('#sw_jidenticon').jdenticon(ShareWriting.getPubKeyHex());
+			$('#sw_jidenticon').attr('alt',ShareWriting.author_name + "'s pub key");
+			$('#sw_jidenticon').attr('title',ShareWriting.author_name + "'s pub key");
+			$('#containter-pubkey').show();
+		}
+	});
+	
+	/* accept key */
+	$('#btn-accept-pubkey').click(function(){
+		$('#sw_author').val('');
+		$('#sw_pass').val('');
+		$('#sw_author_ok').val(ShareWriting.author_name);
+		$('#sw_pubkey_ok').val(ShareWriting.getPubKey());
+		$('#sw_jidenticon_ok').jdenticon(ShareWriting.getPubKeyHex());
+		$('#sw_jidenticon_ok').attr('alt',ShareWriting.author_name + "'s pub key");
+		$('#sw_jidenticon_ok').attr('title',ShareWriting.author_name + "'s pub key");
+		$('#sw_jidenticon_ok').attr('title',ShareWriting.author_name + "'s pub key");
+		$('#alert-authentication').hide();
+		$('#alert-edition').show();
+		$('#form-login').hide();
+		$('#form-publish').show();
+	});
+	
+	/* sign and preview */
+	$('#btn-sign-preview').click(function(){
+		/* TODO: check title, lang, tags and body format */
+		if($('#sw_title').val().length===0) {
+			$('#alert-error-content').html('Title is required.');
+			$('#alert-error-message').show();
+			return;
+		}
+		var article = {
+			'author': ShareWriting.author_name,
+			'pubkey': ShareWriting.getPubKey(),
+			'timestamp': Date.now(),
+			'title': $('#sw_title').val(),
+			'lang': $('#sw_lang').val(),
+			'tags': $('#sw_tags').val(),
+			'body': CKEDITOR.instances.sw_editor.getData()
+		};
+		var signature = ShareWriting.getSignature(JSON.stringify(article));
+		if(signature!==false) {
+			$('#sw_signature').val(signature);
+			var writing = {
+				'version': 1,
+				'algo': "ed25519",
+				'article': article,
+				'signature': signature
+			};
+			console.log(writing);
+			$('#writing-json').val(JSON.stringify(writing));
+			var preview = ShareWriting.getFormattedWriting(writing);
+			$('#writing-preview').html(preview);
+			jdenticon.update('#writing-preview canvas.jdenticon');
+			$('#container-preview').show();
+			window.location = '#container-preview';
+		}
+	});
+	
+});
